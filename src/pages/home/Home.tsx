c637531f54c29edfdd0ceeecb8f535b9025c1623
dylan12manley <dylan.manley@gmail.com>
2025-06-01 16:22:52 -0700

    create Home Component

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className='home'>
+      <h1>Welcome to The Spot</h1>
+      <p>Your one-stop destination for all things community and connection.</p>
+      <p>Join us to explore, share, and engage with like-minded individuals.</p>
+    </div>
+  );
+}
